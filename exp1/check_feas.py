f4f84701b3e563091a0c318d89f0920e6576165d
Tianhao Wu <tianhaowu@usc-guestwireless-upc-newsc10252.usc.edu>
2026-08-11 12:45:29 -0700

    update

--- exp1/check_feas.py
+++ exp1/check_feas.py
@@ -12,8 +12,8 @@ import numpy as np
 import cvxpy as cp
 
 from src.bicycle import KinematicBicycle
-from src.stl import safe_distance_vehicle, safe_distance_walker, clear_intersection, stay_in_lane
 from src.utils import draw_sample_traj, bicycle_to_carla, carla_to_bicycle, COLORS
+from exp1.stl import safe_distance_vehicle, safe_distance_walker, clear_intersection, stay_in_lane
 from exp1.mpc import _ego_state, _build_nominal
 
 import gurobipy as gp
